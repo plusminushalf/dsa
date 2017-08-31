472850b8831c50520787379b6f8a692a2736d55e
Garvit Khatri <garvitdelhi@gmail.com>
2017-08-31 22:00:16 +0530

    adding comments

--- fctrl2/fctrl2.cpp
+++ fctrl2/fctrl2.cpp
@@ -4,6 +4,38 @@
 
 using namespace std;
 
+/*
+ * Question:
+ *
+ * You are asked to calculate factorials of some small positive integers.
+ *
+ * Input:
+ * An integer t, 1<=t<=100, denoting the number of testcases, followed by t lines, each containing a single integer n, 1<=n<=100.
+ *
+ * Output:
+ * For each integer n given at input, display a line with the value of n!
+ *
+ * Example:
+ *
+ * Sample input:
+ * 4
+ * 1
+ * 2
+ * 5
+ * 3
+ *
+ * Sample output:
+ * 1
+ * 2
+ * 120
+ * 6
+ */
+
+/*
+ * function to print vector in reverse order
+ * beacuse numeber 123 is save as 321 in vector form
+ * this is due to the fact that it is easy to multiplication and summation
+ */
 void printVector(vector<int> v) {
     vector<int>::reverse_iterator it;
     for(it = v.rbegin(); it != v.rend(); ++it) {
@@ -11,6 +43,15 @@ void printVector(vector<int> v) {
     }
 }
 
+/*
+ * function to multiply a vector representation of a number a with a unit digit i
+ * Logic:
+ *  11  (carry layer)
+ *  958
+ *   *2
+ *  ---
+ * 1918
+ */
 vector<int> multiply(vector<int> v, int i) {
     vector<int>::iterator it;
     vector<int> r;
@@ -27,6 +68,9 @@ vector<int> multiply(vector<int> v, int i) {
     return r;
 }
 
+/*
+ * function to add to vector representation of numbers
+ */
 vector<int> sum(vector<int> a, vector<int> b) {
     vector<int> s;
     vector<int>::iterator ia, ib;
@@ -58,6 +102,20 @@ vector<int> sum(vector<int> a, vector<int> b) {
     return s;
 }
 
+/*
+ * function to multiply vector implementation of a number with another number which is not in vector representation
+ * logic:
+ *   234
+ *   *23
+ *   ---
+ *   702
+ * +4680
+ * -----
+ *  5382
+ *
+ * I have broken 234*3 part in another function multiply to makes things easier
+ *
+ */
 vector<int> multiplyNum(vector<int> v, int num) {
     vector<int> res, temp;
     int k = 0;
@@ -76,14 +134,21 @@ vector<int> multiplyNum(vector<int> v, int num) {
 int main() {
     // your code goes here
     int t, n;
+    // take number of test cases as inout
     cin >> t;
+    // repeat the loop for all the test cases
     while(t--) {
+        // take the number input
         cin >> n;
+        // vector representation of factorial will be saved in this variable
         vector<int> fact;
+        // we make the value of factorial as 1 by default
         fact.push_back(1);
         for(int i = 2; i <= n; ++i) {
+            // calculating fact as fact(i-1)*i
             fact = multiplyNum(fact, i);
         }
+        // printing vector
         printVector(fact);
         cout << "\n";
     }
